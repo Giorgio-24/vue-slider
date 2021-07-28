fc81934c5acd9d3c513bdac0a74a53336ee084a6
Giorgio Andrei <cipuandrei@libero.it>
2021-07-28 16:05:58 +0200

    classes-for-visibility-based-on-index-added

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-console.log('Vue è connesso', Vue)
 Vue.config.devtools = true;
 
 var root = new Vue(
     {
         el: '#root',
         data: {
+            imageIndex: 0,
+            images: [
+                './img/image1.jpg',
+                './img/image2.jpg',
+                './img/image3.jpg',
+                './img/image4.jpg',
+            ],
 
         },
         methods: {
+            showImage(i) {
+                if (i === this.imageIndex) {
+                    return 'd-inline-block';
+                } else {
+                    return 'd-none';
+                }
 
-        },//^PER LE FUNZIONI
+            },
+        }
     }
 );
