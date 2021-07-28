c14f465396428d208065ef1cd2241e0380c149a1
Giorgio Andrei <cipuandrei@libero.it>
2021-07-28 14:13:34 +0200

    files-created-and-succesfully-linked

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('Vue è connesso', Vue)
+Vue.config.devtools = true;
+
+var root = new Vue(
+    {
+        el: '#root',
+        data: {
+
+        },
+        methods: {
+
+        },//^PER LE FUNZIONI
+    }
+);
